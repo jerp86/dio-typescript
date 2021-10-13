0730c5830d888a3451c5499cf0b36471a7eb36a8
José Eduardo Rodrigues Pinto Jerp86 <jerp4@hotmail.com>
2021-10-13 10:15:26 -0300

    entendendo sobre Interfaces

--- index.ts
+++ index.ts
@@ -1,3 +1,29 @@
 function soma(a: number, b: number) {
   return a + b;
 }
+
+interface IAnimal {
+  name: string;
+  type: "terrestre" | "aquático";
+  runRoar(loudness: number): void;
+  // runRoar: (loudness: number) => void;
+}
+
+interface IFeline extends IAnimal {
+  nightVision: boolean;
+}
+
+const animal: IAnimal = {
+  name: "Elefante",
+  type: "terrestre",
+  runRoar: (loudness) => `${loudness}db`,
+};
+
+animal.runRoar(1);
+
+const feline: IFeline = {
+  name: "Leão",
+  type: "terrestre",
+  runRoar: (loudness) => `${loudness}db`,
+  nightVision: true,
+};
